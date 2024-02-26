bd34d71a79efffe78a28baaabde98bc7d9b80bf1
who i am <raftalya@inbox.ru>
2024-02-26 18:39:18 +0500

    sort-functional

--- src/main/java/com/org/revenge/devstudio/controllers/ShopController.java
+++ src/main/java/com/org/revenge/devstudio/controllers/ShopController.java
@@ -10,8 +10,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class ShopController {
     @GetMapping("/shop")
     public String shop(Model model) {
+        String sortType = "default";
+        //default, a-z, price
+
         model.addAttribute("basketCount", BasketCounter.getProductsCount());
         model.addAttribute("data", Today.getFormattedDate());
+        model.addAttribute("sortType", sortType);
         return "shop";
     }
 }
