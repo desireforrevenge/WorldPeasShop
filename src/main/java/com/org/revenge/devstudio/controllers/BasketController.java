b8268d51a101900cad8d8083f6c8cfd8d0c96f45
who i am <raftalya@inbox.ru>
2024-03-04 17:03:15 +0500

    basket math round fixed

--- src/main/java/com/org/revenge/devstudio/controllers/BasketController.java
+++ src/main/java/com/org/revenge/devstudio/controllers/BasketController.java
@@ -9,10 +9,20 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class BasketController {
     @GetMapping("/basket") //main page
     public String basket(Model model) {
-        model.addAttribute("title", "Мир");
-
         model.addAttribute("basketCount", 1);
+
+        model.addAttribute("typesProducts", 3);
+
+        double num1 = 5.99;
+        double num2 = 6.50;
+        double num3 = 14.95;
+
+        model.addAttribute("totalPrice1", String.format("$%.2f", num1));
+        model.addAttribute("totalPrice2", String.format("$%.2f", num2));
+        model.addAttribute("totalPrice3", String.format("$%.2f", num3));
 
         return "basket";
     }
+
+
 }
